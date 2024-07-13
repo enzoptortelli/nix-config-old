1378caa7f87c805d648256f01af0553dc48b7c72
enzoptortelli <58557898+enzoptortelli@users.noreply.github.com>
2024-07-13 17:46:56 -0300

    Update default.nix

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -4,23 +4,6 @@
 	xsession.windowManager.i3 = {
 		enable = true;
 		config = {
-			modifier = "Mod4";
-			defaultWorkspace = "workspace number 1"; # default workspace at i3 startup
-			bars = []; # remove the default i3 bar
-			window = {
-				border = 6;
-				#titlebar = false;
-			};
-			gaps.inner = 6;
-			colors = {
-				focused = {
-					border = "#ffffff";
-					childBorder = "#cccccc";
-					background = "#222222";
-					text = "#ffffff";
-					indicator = "#555555";	
-				};
-			};
 			startup = [
 				{ command = "systemctl --user restart polybar";
 				  always = true;
